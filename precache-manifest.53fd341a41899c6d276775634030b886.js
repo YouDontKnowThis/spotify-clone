fa6fe862596ee8551d3d9218e093620d0f7a7725
bk dev <fox.physics@outlook.fr>
2020-10-14 07:43:25 +0100

    Updates

---
+++ precache-manifest.53fd341a41899c6d276775634030b886.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad18c9987dce763ab0eed106e1b93684",
+    "url": "/spotify-clone/index.html"
+  },
+  {
+    "revision": "ef3a51a4312b622fad4b",
+    "url": "/spotify-clone/static/css/main.7deca171.chunk.css"
+  },
+  {
+    "revision": "bb3289fec02640f61ff3",
+    "url": "/spotify-clone/static/js/2.ca9edb4e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/spotify-clone/static/js/2.ca9edb4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef3a51a4312b622fad4b",
+    "url": "/spotify-clone/static/js/main.b58de662.chunk.js"
+  },
+  {
+    "revision": "10436233cc4fb7abc8af",
+    "url": "/spotify-clone/static/js/runtime-main.d039cfed.js"
+  }
+]);
